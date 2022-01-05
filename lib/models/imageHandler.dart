b37d9226694e9159d4cd3f7db009b7e122edbf2a
Ahsan <Ahsan.161815@gmail.com>
2022-01-05 23:39:12 +0500

    Fixed unknow bug

--- lib/models/imageHandler.dart
+++ lib/models/imageHandler.dart
@@ -41,7 +41,8 @@ class ImageHandler{
     var length = await imageFile.length();
 
     // string to uri
-    var uri = Uri.parse("http://10.102.130.246:5000/");
+    // var uri = Uri.parse("http://10.102.130.246:5000/");
+    var uri = Uri.parse("http://192.168.1.4:5000/");
 
     // create multipart request
     var request = http.MultipartRequest("POST", uri);
--- lib/pages/response/Response.dart
+++ lib/pages/response/Response.dart
@@ -1,12 +1,14 @@
-import 'package:flutter/material.dart'
-;
+import 'package:flutter/material.dart';
+
 class Response extends StatelessWidget {
   const Response({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     // bool isLoading = true;
+
     var revddata = ModalRoute.of(context)?.settings.arguments;
+
     return Scaffold(
       body: SafeArea(
         child: Center(
